b2f0e39d94c5354e87e56ea7a16fd252cc689b23
Amir Eid <aeid@c2r3p13.42firenze.it>
2024-07-11 17:53:42 +0200

    latest exec

--- execution/execution_routine.c
+++ execution/execution_routine.c
@@ -6,7 +6,7 @@
 /*   By: aeid <aeid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:50:22 by aeid              #+#    #+#             */
-/*   Updated: 2024/07/11 17:52:36 by aeid             ###   ########.fr       */
+/*   Updated: 2024/07/11 17:53:28 by aeid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,12 +64,12 @@ void	ft_execute_routine(t_list *tokens, t_list *env)
 	//THIS IS WORK IN PROGRESS
 	//in this function i should iterate over the list, until pipe or null..
 	//the function will call different functions depending on the type of the token. >> > < << external,
-		builtin
+		//builtin
 	//make sure to do invoke execve in the end of the function,
-		execute first all builtins and redirections first, then
+		//execute first all builtins and redirections first, then
 	//execute the external command.
 	// a process can only hand one command (external or builtin) at a time,
-		with redirections.
+		//with redirections.
 	current = tokens;
 	num_redirs = ft_get_number_of_redirections(tokens);
 	tokendata = (t_tkn_data *)current->content;
